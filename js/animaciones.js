343a52d26fe6c8cf409e10a4ecb16824a4c264cc
d47 <73448973+mcdavidleonardo@users.noreply.github.com>
2024-07-25 08:34:45 -0500

    DME -- Limpieza de repo --

--- js/animaciones.js
+++
@@ -1,9 +0,0 @@
-/*Ejecucion cuando el documento html se haya cargado*/
-document.addEventListener('DOMContentLoaded', function() {
-    const navItems = document.querySelectorAll('.nav-item');
-    navItems.forEach((item, index) => {
-      setTimeout(() => {
-        item.classList.add('show');
-      }, index * 200); // Espaciado de tiempo entre animaciones de cada item
-    });
-  });
